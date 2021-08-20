8b08f1599a36f9918901a2df3f90af375cd20d5f
Halle Dimsdale-Zucker <zhallez@gmail.com>
2021-08-20 10:43:43 -0400

    Add preproc_combined.m script w/in eeg-analysis-scripts folder

---
+++ eeg-analysis-scripts/preproc_combined.m
@@ -0,0 +1,14 @@
+downsample_preproc
+relabel_electrodes
+revalue_events
+s217_split_b2_b3
+rereference
+hpf_preproc
+hpf_for_ica
+identify_bad_channels
+summarize_bad_channels
+epoch_trials
+identify_and_remove_bad_epochs_for_ica
+remove_bad_epochs
+summarize_bad_epochs
+ica 
