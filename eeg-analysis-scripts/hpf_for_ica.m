f39d23d9a81fc4e29f9e40ae1c5e4a13e12ab161
Halle Dimsdale-Zucker <zhallez@gmail.com>
2021-08-24 09:53:33 -0400

    Add high-pass filtering scripts

---
+++ eeg-analysis-scripts/hpf_for_ica.m
@@ -0,0 +1,59 @@
+% Script to highpass filter.
+%
+% Halle R. Dimsdale-Zucker
+
+close all;
+fclose('all');
+clc;
+clear all;
+
+config = eetemp_initialize;
+subjects = config.subjects;
+
+for isub = 1:length(subjects)
+    subj_start = tic;
+    subj_str = subjects{isub};
+    subj_anal_dir = fullfile(config.analyzed_eeg_dir, subj_str);
+
+    set_str_in = 'downsamp_electrode-ids_revalued-events_reref';
+    set_str_out = sprintf('hpf-ica-%0.2g', config.hpf_ica_cutoff);
+
+    % print out info to a diary file
+    diaryname = fullfile(config.logs_dir, sprintf('%s_diaryfile-%s_%s.txt', subj_str, set_str_out, date));
+    diary(diaryname);
+
+    [ALLEEG EEG CURRENTSET ALLCOM] = eeglab; % start eeglab
+
+    for iset = 1:config.num_blocks
+        fname_in = sprintf('%s_eetemp_b%s_%s.set', subj_str, num2str(iset), set_str_in);
+
+        if ~exist(fullfile(subj_anal_dir, fname_in), 'file')
+            fprintf('%s does not exist - continuing.\n', fname_in)
+            continue;
+        end
+
+        EEG = pop_loadset('filename', fname_in,'filepath', subj_anal_dir); % load in dataset
+
+        [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+        EEG = eeg_checkset( EEG );
+
+        % high-pass filter
+        % Luck recommendation
+        % is to remove mean DC offset if have DC data (which I don't think
+        % we do???) but also that it won't hurt if don't have DC data
+        EEG  = pop_basicfilter( EEG,  1:70 , 'Boundary', 'boundary', 'Cutoff',  config.hpf_ica_cutoff,...
+            'Design', 'butter', 'Filter', 'highpass', 'Order',  2, 'RemoveDC', 'on' );
+
+        % save out dataset
+        set_name_out = sprintf('%s_%s', set_str_in, set_str_out); % doesn't contain '.set' file string
+        fname_out = sprintf('%s_eetemp_b%s_%s.set', subj_str, num2str(iset), set_name_out);
+        [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+        EEG = pop_saveset(EEG, 'filename', fname_out, 'filepath', subj_anal_dir);
+        eeglab redraw;
+
+    end % iset
+
+    subj_end = toc(subj_start);
+    fprintf('\nThat took %d minutes and %f seconds.\n',floor(subj_end/60),rem(subj_end,60))
+    diary off;
+end
